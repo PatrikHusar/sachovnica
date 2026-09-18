a69de47bcefd34bbb26836d43ac01e12f6110d75
PatrikHusar <patrik.husar.sk@gmail.com>
2026-09-18 09:02:56 +0200

    created chessboard

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
+const canvas = document.getElementById('chessboard')
+const ctx = canvas.getContext('2d')
 
+const squareSize = 100
+
+for (let column = 0; column < 8; column++) {
+    for (let row = 0; row < 8; row++) {
+        if ((column + row) % 2 === 0) {
+            ctx.fillStyle = 'white'
+        } else {
+            ctx.fillStyle = 'black'
+        }
+        ctx.fillRect(column * squareSize, row * squareSize, squareSize, squareSize)    }
+}
