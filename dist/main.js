5776b29976fd3024944bab2e2840bd8cc8035ac2
PatrikHusar <patrik.husar.sk@gmail.com>
2026-09-18 09:37:50 +0200

    added letters from a to h to chessboard

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,9 @@
 "use strict";
 const canvas = document.getElementById('chessboard');
 const ctx = canvas.getContext('2d');
-const squareSize = 100;
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+const startingDis = 50;
+const squareSize = 70;
 for (let column = 0; column < 8; column++) {
     for (let row = 0; row < 8; row++) {
         if ((column + row) % 2 === 0) {
@@ -10,6 +12,10 @@ for (let column = 0; column < 8; column++) {
         else {
             ctx.fillStyle = 'black';
         }
-        ctx.fillRect(column * squareSize, row * squareSize, squareSize, squareSize);
+        ctx.fillRect(startingDis + column * squareSize, startingDis + row * squareSize, squareSize, squareSize);
     }
+}
+ctx.fillStyle = 'black';
+for (let row = 0; row < 8; row++) {
+    ctx.fillText(letters.at(row), startingDis + row * squareSize + squareSize / 2, squareSize * 8 + startingDis * 1.5);
 }
